388847bf8fe161550512eea25e757bf41736d1c6
Safa Falaqi <sfalaqi@gmail.com>
2021-12-13 16:21:36 +0300

    Initial Commit

---
+++ CoreDataBucketList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataBucketList
+//
+//  Created by Safa Falaqi on 13/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
